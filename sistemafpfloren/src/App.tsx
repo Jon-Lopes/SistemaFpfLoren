fac323af9610ff772e8dafce0fdcfb0726037ac0
Jonatas Siqueira Lopes <jonatas.siq.lopes@gmail.com>
2021-09-03 18:08:44 -0400

    primeiras modificacoes

--- sistemafpfloren/src/App.tsx
+++ sistemafpfloren/src/App.tsx
@@ -1,7 +1,15 @@
+//import { Button } from './components/Button';
+
+import './styles/global.css';
+
 function App() {
   return (
-    <h1>Hello Me</h1>
+    <div>
+      <button style={{ height: '3rem' }}>
+        Teste
+      </button>
+    </div>
   );
 }
-
+//<div>//<Button color="green"> Salvar  </Button> //<Button color="blue">  Alterar  </Button> //</div>
 export default App;
