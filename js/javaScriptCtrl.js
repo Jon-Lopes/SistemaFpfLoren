8077a04972ba03f04d338585017b6b8a256924f4
Jonatas Siqueira Lopes <jonatas.siq.lopes@gmail.com>
2021-09-04 21:17:08 -0400

    paginas adicionadas

--- js/javaScriptCtrl.js
+++ js/javaScriptCtrl.js
@@ -4,15 +4,15 @@ var editar1 = document.querySelector('#editar1');
 var excluir1 = document.querySelector('#excluir1');
 var simular1 = document.querySelector('#simular1');
 
-function eita() {
-    var teste;
-    var r = confirm("Voce quer deletar?");
-    if (r == true) {
-        teste = "ok";
-    } else {
-        teste = "nao";
-    }
-    console.log(teste);
-};
 
-eita();
+// function eita() {
+//     var teste;
+//     var r = confirm("Voce quer deletar?");
+//     if (r == true) {
+//         teste = "ok";
+//     } else {
+//         teste = "nao";
+//     }
+//     console.log(teste);
+// };
+// eita();
