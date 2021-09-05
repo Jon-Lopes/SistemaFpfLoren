baa5ec72600b9d43b4a15aee505aa2a0ee4c8e67
Jonatas Siqueira Lopes <jonatas.siq.lopes@gmail.com>
2021-09-04 23:54:07 -0400

    Implementando primeiras funcionalidades

--- js/javaScriptCtrl.js
+++ js/javaScriptCtrl.js
@@ -4,7 +4,18 @@ var editar1 = document.querySelector('#editar1');
 var excluir1 = document.querySelector('#excluir1');
 var simular1 = document.querySelector('#simular1');
 
+var enviar1 = document.querySelector('#enviar1');
+var simular2 = document.querySelector('#simular2');
 
+
+//1º cenario, preenche formulario, nomeProj ainda nao existe no BD
+function registry() {
+    console.log('esta funcionando');
+}
+
+cadastrar1.addEventListener("click", function () { registry() }, 1000);
+editar1.onclick = function () { registry() };
+//2º cenario,
 // function eita() {
 //     var teste;
 //     var r = confirm("Voce quer deletar?");
